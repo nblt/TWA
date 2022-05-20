e7871f88577bdcc17703f670bafc320ad8ac5172
nblt <ltwtzjh@126.com>
2022-05-20 21:13:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ lr=2
 end=101
 wd_psgd=0.00001
 CUDA_VISIBLE_DEVICES=$device python -u train_twa.py --epochs 10 --datasets $datasets \
-        --opt SGD --extract Schmidt --schedule step --noise 0.0 --accumulate 1 \
+        --opt SGD --extract Schmidt --schedule step \
         --lr $lr --params_start 0 --params_end $end --train_start -1 --wd $wd_psgd \
         --batch-size 128  --arch=$model  \
-        --save-dir=$DST/checkpoints  --log-dir=$DST --log-name=condense
+        --save-dir=$DST/checkpoints  --log-dir=$DST
 
 
 ################################ ImageNet ################################
@@ -36,17 +36,17 @@ wd_psgd=0.00001
 lr=0.3
 DST=save_resnet18
 CUDA_VISIBLE_DEVICES=$device python -u train_twa.py --epochs 2 --datasets $datasets \
-        --opt SGD --extract Schmidt --schedule step --noise 0.0 --accumulate 1 --worker 8 \
+        --opt SGD --extract Schmidt --schedule step --worker 8 \
         --lr $lr --params_start 0 --params_end 301 --train_start -1 --wd $wd_psgd \
         --batch-size 256  --arch=$model  \
-        --save-dir=$DST  --log-dir=$DST --log-name from_begin
+        --save-dir=$DST  --log-dir=$DST 
 
 # TWA 90+1
 wd_psgd=0.00001
 lr=0.03
 DST=save_resnet18
 CUDA_VISIBLE_DEVICES=$device python -u train_twa.py --epochs 1 --datasets $datasets \
-        --opt SGD --extract Schmidt --schedule linear --noise 0.0 --accumulate 1 --worker 8 \
+        --opt SGD --extract Schmidt --schedule linear --worker 8 \
         --lr $lr --params_start 301 --params_end 451 --train_start -1 --wd $wd_psgd \
         --batch-size 256  --arch=$model  \
-        --save-dir=$DST  --log-dir=$DST --log-name from_begin
+        --save-dir=$DST  --log-dir=$DST 
